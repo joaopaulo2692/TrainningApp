c1cf071f4526f28f591222b12566f8168291cc2a
joaop <joaopaulo2692@gmail.com>
2024-07-20 18:07:04 -0300

    Adicionar arquivos de projeto.

---
+++ TrainningApp/Program.cs
@@ -0,0 +1,37 @@
+using MudBlazor.Services;
+using TrainningApp.Client.Pages;
+using TrainningApp.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add MudBlazor services
+builder.Services.AddMudServices();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(TrainningApp.Client._Imports).Assembly);
+
+app.Run();
